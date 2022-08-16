35eba215c4a72d216763383a228c806b212adada
dez0514 <2573433803@qq.com>
2022-08-16 18:03:24 +0800

    md

--- src/utils/marked.ts
+++ src/utils/marked.ts
@@ -1,5 +1,6 @@
 import highlightJs from 'highlight.js'
 import { marked } from 'marked'
+import { getOffsetTop } from './dom'
 // 解析markdown
 export const formartMd = (md: string) => {
   const renderer = new marked.Renderer()
@@ -24,3 +25,83 @@ export const formartMd = (md: string) => {
   const html = marked(md)
   return html
 }
+
+export const formatTagByHtml = (html: string) => {
+  return html
+    .replace(/<(h[1-2])(([\s\S])*?)<\/h[1-2]>/g, (el) => {
+      return `
+    <div class="banner-tag primary banner-tag__left">
+      <div class="banner-tag__content">
+        <div class="banner-tag__wrap">${el}</div>
+      </div>
+    </div>
+    `
+    })
+    .replace(/<h3(([\s\S])*?)<\/h3>/g, (el) => {
+      return `
+    <div class="md-h3-container">
+    ${el}
+    </div>
+    `
+    })
+}
+export interface MdTitle {
+  nodeName: string
+  text: string
+  id: string
+  scrollTop: number
+}
+export const getMdTitleList = (container: Element, tags = 'h1, h2, h3'): MdTitle[] => {
+  const nodeList = Array.from(container.querySelectorAll(tags))
+  const headerH = (document.querySelector('.main-header')?.getBoundingClientRect().height || 0) + 15
+  return nodeList.map((node: any) => ({
+    nodeName: node.nodeName.toLocaleLowerCase(),
+    text: node.innerText,
+    id: node.id,
+    scrollTop: getOffsetTop(document.getElementById(node.id) as Element) - headerH
+  }))
+}
+
+// // sidebar hook
+// export const useSidebar = (
+//   list: MdTitle[],
+//   fallback?: () => void
+// ): [
+//   {
+//     id: string
+//     title: string
+//     isSub: boolean
+//   }[],
+//   number,
+//   (item: SideTitleItem) => void
+// ] => {
+//   const state = useRef({
+//     isAnimate: false
+//   })
+//   const [activeIndex, setActiveIndex] = useState(0)
+//   const sideList = getSideList(list)
+//   const onChange = (item: SideTitleItem) => {
+//     const obj = list.find((v) => v.id === item.id) as MdTitle
+//     fallback && fallback()
+//     state.current.isAnimate = true
+//     setScrollTop(obj.scrollTop, {
+//       animate: true,
+//       complete: () => {
+//         state.current.isAnimate = false
+//       }
+//     })
+//   }
+//   useScroll((scrollTop) => {
+//     if (state.current.isAnimate) {
+//       return
+//     }
+//     const oldArr = list.map((item) => item.scrollTop)
+//     const arr = [...oldArr].reverse()
+//     const top = arr.find((top) => scrollTop > top)
+//     let index = oldArr.findIndex((t) => top === t) + 1
+//     index = index < 0 ? 0 : index
+//     index = index > oldArr.length - 1 ? oldArr.length - 1 : index
+//     setActiveIndex(index)
+//   })
+//   return [sideList, activeIndex, onChange]
+// }
--- src/views/article/articleDetail.vue
+++ src/views/article/articleDetail.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="detail-page">
     <div class="title">{{ detailInfo && detailInfo.title }}</div>
-    <div class="stitle">{{ detailInfo && detailInfo.extra_title }}</div>
-    <div class="tags">{{ detailInfo && detailInfo.tags }}</div>
-    <div class="banner">
+    <div class="extra_info">
+      <div class="extra-box">{{ detailInfo && detailInfo.extra_title }}</div>
+      <a-divider type="vertical" />
+      <div class="extra-box">{{ detailInfo && detailInfo.tags }}</div>
+      <a-divider type="vertical" />
+      <div class="extra-box">
+        {{ (detailInfo && detailInfo.update_time) ? `更新时间：${detailInfo.update_time}` : `创建时间：${detailInfo && detailInfo.create_time}` }}
+      </div>
+    </div>
+    <div class="banner" :style="{ background: `url(${detailInfo && detailInfo.banner}) no-repeat`, backgroundPosition: 'center center', backgroundSize: 'cover' }">
       <img :src="detailInfo && detailInfo.banner" alt="">
     </div>
-    <div class="desc md-container">
-      <!-- <div>{{ detailInfo && detailInfo.content }}</div> -->
+    <div class="detail-desc md-container">
       <div v-html="detailInfo && detailInfo.content"></div>
     </div>
   </div>
@@ -44,19 +50,48 @@ onMounted(() => {
 .detail-page {
   padding: 0 20px;
 }
+.extra_info {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 50px;
+  .extra-box {
+    margin: 0 5px;
+  }
+}
 .title {
   text-align: center;
-  font-size: 22px;
+  font-size: 24px;
 }
-.stitle {
-  text-align: center;
+.banner {
+  overflow: hidden;
+  position: relative;
+  height: 350px;
+  width: 100%;
+  background-color: #f0f2f7;
+  border-radius: 10px;
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, .6);
+    filter: blur(20px);
+  }
+  img {
+    z-index: 10;
+    position: absolute;
+    right: 50px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: block;
+    max-width: 200px;
+    max-height: 200px;
+  }
 }
-.tags {
-  text-align: center;
-}
-.banner img {
-  display: block;
-  margin: 0 auto;
-  max-height: 200px;
+.detail-desc {
+  margin-top: 20px;
 }
 </style>
