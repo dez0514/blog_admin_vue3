fa6623a6f3c7e8d74e78b6545ee58bb8426e964f
dez0514 <2573433803@qq.com>
2022-08-28 13:38:33 +0800

    test

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,25 +1,28 @@
 <template>
+  <div style="padding: 0 20px;">
     <div>home</div>
     <a-button type="primary" @click="test"> Primary</a-button>
     <a-alert message="好可惜，我也不会" type="success" />
-    <div>{{filter + '' + nextId}}</div>
-    <div v-for="(item,index) in todos" :key="index">{{item.text}}</div>
+    <div>{{ filter + '' + nextId }}</div>
+    <div v-for="(item, index) in todos" :key="index">{{ item.text }}</div>
     <a-button type="primary" @click="handleChangeMeta">修改router.meta中的参数</a-button>
-    <div>comments:{{comments}}</div>
+    <div>comments:{{ comments }}</div>
+  </div>
 </template>
 <script setup lang="ts">
 import { todoStore } from '../../store/pinia'
 import { storeToRefs } from 'pinia'
 // import { getData } from '../../api'
 const store = todoStore()
-const { todos,filter,nextId,comments } = storeToRefs(store)
+const { todos, filter, nextId, comments } = storeToRefs(store)
 const test = () => {
-    console.log(store)
-    store.addTodo('test1')
+  console.log(store)
+  store.addTodo('test1')
 }
 const handleChangeMeta = () => {
   store.updateComments(25)
   console.log(comments)
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
